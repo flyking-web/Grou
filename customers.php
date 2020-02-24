2961989a39b08a72736af13c1ea6e084c637ae04
flyking-web <60378962+flyking-web@users.noreply.github.com>
2020-02-24 22:40:49 +0300

    Add files via upload

---
+++ customers.php
@@ -0,0 +1,30 @@
+<?php
+require 'config.php';
+require 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"0>
+    <meta name="viewport" content="width=initial width-width,initial scale=0">
+    <title>Definition contact</title>
+</head>
+<body>
+<div class="col col-sm-4 col-md-4 col-lg-4 col-xl-4">
+    <h5></h5>
+    <a href="">
+        <img src="images/twitter-night.jpg" alt="s" style="height:80px;width:80px;padding-top:20px;padding-bottom:10px">
+    </a>
+    <a href="">
+        <img src="images/ig.jpg" alt="insta" style="height:80px;width:80px;padding-top:20px;padding-bottom:10px" >
+    </a>
+    <a href="">
+        <img src="images/fb.jpg" alt="fb" style="height:80px;width:80px;padding-top:20px;padding-bottom:10px" >
+    </a>
+</div>
+<div class="col col-sm-4 col-md-4 col-lg-4 col-xl-4">
+    <h5>Contact-Us</h5>
+    <p><i>  Phone:0734567897</i> <br></p>
+    <p><i>Email:housing101@gmail.com</i></p>
+</div>
+</body>
